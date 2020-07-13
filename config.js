ddd66a6850882b8b70b6a8e3e135fe51e0fb0a9f
fthrrsi <66994875+fthrrsi@users.noreply.github.com>
2020-07-13 16:09:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   subdomain: "api",
-  consumer_key: "ENTER YOUR KEY HERE",
-  consumer_secret: "ENTER YOUR KEY HERE",
-  access_token_key: "ENTER YOUR KEY HERE",
-  access_token_secret: "ENTER YOUR KEY HERE"
+  consumer_key: "kC8RvOGThOCmHOQSL05565Ddz",
+  consumer_secret: "UBPPdXNNoZyhx61O01d9L55KyzCBcfbyTIjPkldtCYtOOitgg3",
+  access_token_key: "1281470835648106499-cioMGeJEf8EqR8TTZra1qNOus4W1F0",
+  access_token_secret: "uwN889yoYnhOhkfNIiPqLamnosSETbDJBX38ihT44v5Lz"
 };
