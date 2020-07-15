4232b60e04cb843e484a89f0eb1fac568d5bf5a1
fthrrsi <66994875+fthrrsi@users.noreply.github.com>
2020-07-15 08:07:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ const replyToTweet = tweet => {
 };
 
 setInterval(() => {
-  const tweet = client.get("search/tweets", { q: "#2103 -filter:retweets AND -filter:replies", count: "1" })
+  const tweet = client.get("search/tweets", { q: "#sedih -filter:retweets AND -filter:replies", count: "1" })
     .then(tweet => replyToTweet(tweet.statuses[0]))
 }, 300000);
